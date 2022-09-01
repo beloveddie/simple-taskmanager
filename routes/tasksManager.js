b0e03b384f10d3c6f1f8ac547f3522997393f7b1
Eddiebee <edbeeotudor@gmail.com>
2022-09-01 17:20:33 +0100

    Add route controllers

--- routes/tasksManager.js
+++ routes/tasksManager.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const taskController = require("../controllers/tasksController");
 
 router
   .route("/")
-  .get((req, res) => {
-    res.json({ message: "GET coming soon!" });
-  })
-  .post((req, res) => {
-    res.json({ message: "POST coming soon!" });
-  })
-  .put((req, res) => {
-    res.json({ message: "PUT coming soon!" });
-  })
-  .delete((req, res) => {
-    res.json({ message: "DELETE coming soon!" });
-  });
+  .get(taskController.getAllTasks)
+  .post(taskController.addTask)
+  .put(taskController.updateTask)
+  .delete(taskController.deleteTask);
 
-router.get("/:id", (req, res) => {
-  res.json({ message: `GET by ID ${req.params.id} coming soon!` });
-});
+router.route("/:id").get(taskController.getTaskById);
+
 module.exports = router;
