ee97c06880e20332447e5446a45f4aa77e347a39
Eddiebee <edbeeotudor@gmail.com>
2022-09-01 15:49:27 +0100

    Serve static files

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("simpe task manager API");
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+// use CORs (OPEN)
+app.use(cors());
+// built-in middleware for json
+app.use(express.json());
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
