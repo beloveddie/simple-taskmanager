cd5775ae2b58b290181973e58f72a00304f1c23e
Eddiebee <edbeeotudor@gmail.com>
2022-09-01 16:11:56 +0100

    Refactor route for task-manager API

--- routes/tasksManager.js
+++ routes/tasksManager.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
-  res.json({ message: "coming soon!" });
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.json({ message: "GET coming soon!" });
+  })
+  .post((req, res) => {
+    res.json({ message: "POST coming soon!" });
+  })
+  .put((req, res) => {
+    res.json({ message: "PUT coming soon!" });
+  })
+  .delete((req, res) => {
+    res.json({ message: "DELETE coming soon!" });
+  });
 
 module.exports = router;
